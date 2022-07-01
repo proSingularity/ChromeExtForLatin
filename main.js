61c281483e538281c0cdc5f42d5b2afadf5c1837
Mirco Kraenz <software@kraenz.eu>
2022-07-01 22:16:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-searchUrbanDict = function(word){
-  var query = word.selectionText;
-  chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
-  chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + query + "&title=Special%3ASearch&go=Go"});
-};
-
-chrome.contextMenus.create({
-  title: "Search in UrbanDictionary",
-  contexts:["selection"],
-  onclick: searchUrbanDict
-});
-
 searchWikipedia = function(word){
   var query = word.selectionText;
-  chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + query + "&title=Special%3ASearch&go=Go"});
+  chrome.tabs.create({url: `https://en.wiktionary.org/wiki/${query}#Latin`;
 };
 
 chrome.contextMenus.create({
