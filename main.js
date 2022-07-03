4a117eb4d16a5d413a85d17ed0af1c50353e2991
Mirco Kraenz <software@kraenz.eu>
2022-07-03 13:28:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 searchWikipedia = function(word){
   var query = word.selectionText;
-  chrome.tabs.create({url: `https://en.wiktionary.org/wiki/${query}#Latin`});
+  chrome.tabs.create({url: `https://en.wiktionary.org/wiki/${query?.toLowerCase()}#Latin`});
 };
 
 chrome.contextMenus.create({
